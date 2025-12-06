558ea27284ce26f5faf1410d9dc03ab4b925dd0f
naveen-dev-ent <naveen.kumar.dev@outlook.com>
2025-12-07 01:03:52 +0530

    Binary Search

---
+++ python/learn/binary_search.py
@@ -0,0 +1,15 @@
+def binary_search(arr, target):
+	left = 0
+	right = len(arr) -1
+	while (left <= right):
+		mid = (left + right)//2
+		if arr[mid] == target:
+			return mid
+		elif arr[mid] < target:
+			left = mid+1
+		else:
+			right = mid-1
+	return
+
+arr = [10, 20, 30, 40, 50, 60]
+print(binary_search(arr, 40))
